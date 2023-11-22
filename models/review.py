ca0db3637eae577f25dac799ebab8692fa3e43de
eungobs <eungobs@gmail.com>
2023-11-22 07:42:07 -0800

    review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+class Review(BaseModel, Base):
+    """ Review class representing the reviews table """
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', backref='reviews')
+    place = relationship('Place', backref='reviews')
