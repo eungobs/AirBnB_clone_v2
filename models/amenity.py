23c336fdba658b4599d443c18f21153d1ada822d
eungobs <eungobs@gmail.com>
2023-11-22 07:46:00 -0800

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+class Review(BaseModel, Base):
+    """ Review class representing the reviews table """
 
-class Amenity(BaseModel):
-    name = ""
+    __tablename__ = 'reviews'
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    # Relationships
+    user = relationship('User', backref='reviews')
+    place = relationship('Place', backref='reviews')
